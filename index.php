bcff6a351d9645169bec952e26da9d9540372b8b
Helio-A-Tome <netsharkinformatica@gmail.com>
2022-02-22 09:15:13 -0300

    modificando index e apaga css

--- index.php
+++ index.php
@@ -7,6 +7,24 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="formu">
+    <form method="POST" action="enviar.php" enctype="multpart/form-data">
+    <label for="">
+    Nome:
+    <input type="text" name="nome" id="">
+    </label><br/><br/>
+    <label for="">
+    Email:<input type="email" name="email" id="">
+    </label><br/><br/>
+    <label>
+    Senha:<input type="password" name="senha" id="">
+    </label><br/><br/>
+    <label for="">
+    Documentos:<input type="file" name="arquivos">
     
+    </label><br/><br/>
+    <input type="submit" value="enviar">
+    </form>
+    </div>
 </body>
 </html>
