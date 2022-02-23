18675dad6df5cfe3f64ebc3a4267e6b1925841cb
Helio-A-Tome <netsharkinformatica@gmail.com>
2022-02-23 12:47:59 -0300

    modificado a principio o css e o index

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+    <title>formulario em php</title>
 </head>
 <body>
+<header>
+<h1>Simples formulario em PHP</h1>
+<nav>
+<ul>
+<li>
+</li>
+</ul>
+</nav>
+</header>
+<section>
+<div class="sessao">
+</div>
+</section>
     <div class="formu">
     <form method="POST" action="enviar.php" enctype="multipart/form-data">
     <label for="">
